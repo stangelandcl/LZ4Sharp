78bcddb2b16696fc5dcb60194e0fc06e416970a6
Clayton Stangeland <clayton.stangeland@gmail.com>
2012-02-23 09:31:43 -0600

    Add missing files

---
+++ LZ4Sharp/LZ4DecompressorFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LZ4Sharp
+{
+    public static class LZ4DecompressorFactory
+    {
+        public static ILZ4Decompressor CreateNew()
+        {
+            if (IntPtr.Size == 4)
+                return new LZ4Decompressor32();
+            return new LZ4Decompressor64();
+        }
+    }
+}
